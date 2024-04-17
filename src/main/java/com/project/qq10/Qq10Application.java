9f79ebb0a73b77c1ef89143458377f797a67cd6c
artwoojin <artwoojin@gmail.com>
2024-04-17 20:34:50 +0900

    first commit

---
+++ src/main/java/com/project/qq10/Qq10Application.java
@@ -0,0 +1,13 @@
+package com.project.qq10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qq10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qq10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/qq10/Qq10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.qq10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qq10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
